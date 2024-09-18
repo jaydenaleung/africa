5bb3020db72b5c31bb3f581a6311cabf24464a8e
Jayden Leung <jaydenaleung@gmail.com>
2024-09-18 18:20:40 -0400

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Jayden Leung on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
